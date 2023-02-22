b948c1fafa6d8e11485ab7fa767678e56753a17f
Angana Ganguly <angana_ganguly@persistent.com>
2023-02-22 11:06:51 +0530

    added-2-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import Product from "./pages/Product";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import React from "react";
+import { Router } from "@reach/router";
 
 const App = () => {
   return <Home/>;
